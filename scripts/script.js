e2cd2ff4ca0660ae286a827c5ac2008bd762f10a
necaprogramer <nissimbara@outlook.com>
2022-08-13 21:03:21 +0200

    Enable api to get more restaurants, as to lessen api requests. Add loading animation once the user is at end of the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 import { API_URL, BEARER_TOKEN } from './api-config.js';
 
-const TOTAL_RESTAURANTS = 1000;
+const RESTAURANTS_PER_SCROLL = 15;
+const LIMIT_OF_FETCHED_RESTAURANTS = 50;
+
+let arrayOfRestaurants = [];
+let pageScrolledCounter = 1;
+let totalRestaurants = 0;
 
 let containerDiv = document.getElementById('container');
 let buttonBurgers = document.getElementById('burgers');
@@ -10,13 +15,16 @@ let buttonJapanese = document.getElementById('japanese');
 let loader = document.getElementById('loader');
 hideLoader();
 
-let limitOfShownRestaurants = 15;
-let offsetOfShownRestaurants = 0;
-let totalShownRestaurants = 0;
 
-buttonBurgers.addEventListener('click', () => {
-    getRequest('burgers', offsetOfShownRestaurants, limitOfShownRestaurants);
-    totalShownRestaurants += 15;
+let offsetOfFetchedRestaurants = 0;
+let totalShownRestaurants = 0;
+let elementsOnPage = 0;
+
+buttonBurgers.addEventListener('click', async () => {
+    await getRequest('burgers', offsetOfFetchedRestaurants, LIMIT_OF_FETCHED_RESTAURANTS);
+    elementsOnPage = arrayOfRestaurants.slice(0, RESTAURANTS_PER_SCROLL);
+    createRestaurantCardsFromArray(elementsOnPage);
+    totalShownRestaurants += RESTAURANTS_PER_SCROLL;
     infiniteScroll('burgers');
 });
 
@@ -28,41 +36,45 @@ buttonJapanese.addEventListener('click', () => {
     getRequest('japanese');
 });
 
-function infiniteScroll(category){
-    window.addEventListener('scroll', () =>{
-        if(window.innerHeight + window.pageYOffset >= document.body.offsetHeight){
-            showLoader();
-            setTimeout(() => {
-                if(totalShownRestaurants != TOTAL_RESTAURANTS){
-                    offsetOfShownRestaurants += 15;
-                    getRequest(category, offsetOfShownRestaurants, limitOfShownRestaurants);
-                    totalShownRestaurants += 15;
-                }
-                hideLoader();
-            }, 800)
-        }
+function createRestaurantCardsFromArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        createRestaurantCard(array[i].name, array[i].image_url, array[i].rating, array[i].price, array[i].url);
+    }
+}
+
+function infiniteScroll(category) {
+        window.addEventListener('scroll', () => {
+            if (document.documentElement.scrollHeight === window.pageYOffset + window.innerHeight) {
+                showLoader();
+                setTimeout(() => {
+                    if (totalShownRestaurants <= totalRestaurants) {
+                        let startingScrollIndex = RESTAURANTS_PER_SCROLL * pageScrolledCounter;
+                        elementsOnPage = arrayOfRestaurants.slice(startingScrollIndex, startingScrollIndex + RESTAURANTS_PER_SCROLL);
+                        createRestaurantCardsFromArray(elementsOnPage);
+                        totalShownRestaurants += RESTAURANTS_PER_SCROLL;
+                        if ((arrayOfRestaurants.length - startingScrollIndex) < RESTAURANTS_PER_SCROLL) {
+                            getRequest(category, startingScrollIndex, LIMIT_OF_FETCHED_RESTAURANTS);
+                        }
+                    }
+                    pageScrolledCounter++;
+                    hideLoader();
+                }, 500)
+            }
     });
 }
 
-async function getRequest(category, offset, limit){
-    try{
-        const res = await axios.get(API_URL + `&categories=${category}&offset=${offset}&limit=${limit}`, {headers:{Authorization: `Bearer ${BEARER_TOKEN}`}});
-        getData(res);
+async function getRequest(category, offset, limit) {
+    try {
+        const res = await axios.get(API_URL + `&categories=${category}&offset=${offset}&limit=${limit}`, { headers: { Authorization: `Bearer ${BEARER_TOKEN}` } });
+        arrayOfRestaurants.push(...res.data.businesses);
+        totalRestaurants = res.data.total;
     }
-    catch(e){
+    catch (e) {
         console.log(e);
     }
 }
 
-function getData(result){
-    console.log(result.data.businesses);
-    let array = result.data.businesses;
-    array.forEach(restaurant => {
-        createRestaurantCard(restaurant.name, restaurant.image_url, restaurant.rating, restaurant.price, restaurant.url);
-    });
-}
-
-function createRestaurantCard(restaurantName, restaurantImage, restaurantRating, restaurantPrice, restaurantLink){
+function createRestaurantCard(restaurantName, restaurantImage, restaurantRating, restaurantPrice, restaurantLink) {
     var restaurantDiv = document.createElement('div');
     containerDiv.appendChild(restaurantDiv);
     restaurantDiv.classList.add('restaurant-container');
@@ -92,12 +104,22 @@ function createRestaurantCard(restaurantName, restaurantImage, restaurantRating,
     link.innerText = 'Link to restaurant website';
 }
 
-function hideLoader(){
+//#region helper methods
+function debounce(func, timeout = 200) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+function hideLoader() {
     loader.classList.remove('show');
     loader.classList.add('hide');
 }
 
-function showLoader(){
+function showLoader() {
     loader.classList.remove('hide');
     loader.classList.add('show');
 }
+//#endregion
